330da483e11bbf01c5ff15a94e4ed306fd5f920d
Mikael Weiss <Campingmikael@icloud.com>
2021-03-04 19:30:32 -0700

    Changed it to a workspace so that SwiftUI previews work

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Mikael Weiss on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
