4d77ed6642669d5f38f450e50b844918403b11cb
Mikael Weiss <Campingmikael@icloud.com>
2021-03-04 20:09:16 -0700

    moved things around

---
+++ SwiftUI/Visuals.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct This: View {
+    var body: some View {
+        Color.blue
+    }
+}
+
+struct This_Previews: PreviewProvider {
+    static var previews: some View {
+        This()
+    }
+}
