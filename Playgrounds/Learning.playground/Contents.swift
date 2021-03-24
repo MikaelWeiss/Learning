eed82e79de3d658bf8da2478cd251892b995dc1e
Mikael Weiss <Campingmikael@icloud.com>
2021-03-23 21:26:39 -0600

    Playing around with dates

--- Playgrounds/Learning.playground/Contents.swift
+++ Playgrounds/Learning.playground/Contents.swift
@@ -11,14 +11,14 @@ enum RecurrenceEnd {
 
 let thing: RecurrenceEnd = .rightAway
 
-switch thing {
-case .date(_):
-    print("🔥")
-case .occurrenceCount(let count):
-    print(count)
-case .never:
-    print("🔥")
-}
+//switch thing {
+//case .date(_):
+//    print("🔥")
+//case .occurrenceCount(let count):
+//    print(count)
+//case .never:
+//    print("🔥")
+//}
 
 
 extension Date {
@@ -37,14 +37,56 @@ extension Date {
     }
 }
 
-print(Date().lastDayOfYear()! <= Date.lastDayOfYear(2020))
+//print(Date().lastDayOfYear()! <= Date.lastDayOfYear(2020))
 
 let arr = [1,3,2]
-print(arr.sorted())
+//print(arr.sorted())
 
 
 let date2020 = Calendar.current.date(from: DateComponents(year: 2020))
 let date2021 = Calendar.current.date(from: DateComponents(year: 2021))
 let date2022 = Calendar.current.date(from: DateComponents(year: 2022))
 var dateArr = [date2021!, date2022!, date2020!]
-print(dateArr.sorted(by: <))
+//print(dateArr.sorted(by: <))
+
+extension Date {
+    func numberOfDaysAgo(_ days: UInt16) -> Date {
+        let cal = Calendar.current
+        let day = cal.component(.day, from: self)
+        let components = DateComponents(day: day - Int(days))
+        return cal.date(from: components)!
+    }
+}
+
+var date = Date().numberOfDaysAgo(5)
+date.addTimeInterval(-1)
+let hourAndMinute = Calendar.current.dateComponents([.hour, .minute], from: date)
+//print(hourAndMinute)
+
+extension Date {
+    func adding(days: Int16) -> Date {
+        let cal = Calendar.current
+        let day = cal.date(byAdding: .day, value: Int(days), to: self)
+        return day!
+    }
+    
+    static func intervalOf(component: Calendar.Component, from startDate: Date, to endDate: Date) -> Int {
+        let cal = Calendar.current
+        let startDate = cal.startOfDay(for: startDate)
+        let endDate = cal.startOfDay(for: endDate)
+        
+        guard let start = cal.ordinality(of: component, in: .era, for: startDate) else { return 0 }
+        guard let end = cal.ordinality(of: component, in: .era, for: endDate) else { return 0 }
+        
+        return end - start
+    }
+}
+
+let oneWeekAgo = Date().adding(days: -6)
+let today = Date()
+
+let days = Date.intervalOf(component: .day, from: oneWeekAgo, to: today)
+print(days)
+
+let val = (days % 2)
+print(val)
