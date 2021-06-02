1e7d002d4860cac539d9f02b78bd3636b82647bf
Mikael Weiss <campingmikael@icloud.com>
2021-06-02 14:40:14 -0600

    Scale to fit stuff

--- Learning/ContentView.swift
+++ Learning/ContentView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let originalWidth: CGFloat = 332
+    private let originalHeight: CGFloat = 212
+    
     var body: some View {
-        GeometryReader { geo in
-            let viewWidth: CGFloat = 332
-            let scaleFactor = geo.size.width / viewWidth
+        VStack(spacing: 0) {
+            Circle()
             Color.blue
-                .frame(width: viewWidth, height: 212)
-                .scaleEffect(scaleFactor, anchor: .center)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-        }.padding()
+                .widthScaled(originalWidth: originalWidth, originalHeight: originalHeight)
+                .border(Color.red, width: 5)
+            Circle()
+        }
+    }
+}
+
+extension View {
+    func widthScaled(originalWidth: CGFloat, originalHeight: CGFloat) -> some View {
+        modifier(WidthScaled(originalWidth: originalWidth, originalHeight: originalHeight))
+    }
+}
+
+struct WidthScaled: ViewModifier {
+    let originalWidth: CGFloat
+    let originalHeight: CGFloat
+    @State var frame: CGSize
+    private var scaleFactor: CGFloat {
+        frame.width / originalWidth
+    }
+    
+    init(originalWidth: CGFloat, originalHeight: CGFloat) {
+        self.originalWidth = originalWidth
+        self.originalHeight = originalHeight
+        self.frame = CGSize(width: originalWidth, height: originalHeight)
+    }
+    
+    func body(content: Content) -> some View {
+        GeometryReader { geo in
+            makeView(geo, content: content)
+        }
+            .frame(height: originalHeight * scaleFactor)
+    }
+    
+    private func makeView(_ geo: GeometryProxy, content: Content) -> some View {
+        print(geo.size.width, geo.size.height)
+
+        DispatchQueue.main.async { self.frame = geo.size }
+        
+        return content
+            .frame(width: originalWidth, height: originalHeight)
+            .scaleEffect(geo.size.width / originalWidth, anchor: .leading)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
